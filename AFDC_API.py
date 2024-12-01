21b913c993717adc28d967cc768a1e65256c5505
JingChengGu <jaesongu@gmail.com>
2024-11-30 17:02:52 -0800

    dealing with sensitive API key

--- AFDC_API.py
+++ AFDC_API.py
@@ -1,29 +1,26 @@
+from dotenv import load_dotenv
+import os
 import requests
 import pandas as pd
 
-# Define the API endpoint and your API key
-api_key = '7oh3YCkkA1Bpea6GzbgeBQ5RD2EygtrVdcRppXw8'
+# Load environment variables from .env
+load_dotenv()
+
+# Get the API key from the environment
+api_key = os.getenv('NREL_API_KEY')
+if not api_key:
+    raise ValueError("API key is missing. Set the NREL_API_KEY environment variable in the .env file.")
+
+# Define the API endpoint
 url = f'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?&api_key={api_key}'
 
 # Make the GET request to the API
 response = requests.get(url)
-
-# Check if the request was successful
 if response.status_code == 200:
-    # Get the data in JSON format
     data = response.json()
-
-    # Extract relevant data from the response
     fuel_stations = data['fuel_stations']
-
-    # Convert the data into a pandas DataFrame
     df = pd.json_normalize(fuel_stations)
-
-    # Save the DataFrame to a CSV file
-    df.to_csv('alternative_fuels_data.csv', index=False)
-
-
+    df.to_csv('datasets/alternative_fuels_data.csv', index=False)
     print('Data successfully saved to alternative_fuels_data.csv')
 else:
     print(f'Error: {response.status_code}')
-
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # DSC180A-SDGE-Q1
+
+
+### Setting Up API Keys
+
+1. Create a `.env` file in the root directory of the project with the following content:
+    NREL_API_KEY=your_nrel_api_key_here
+
+2. Install dependencies:
+```bash
+pip install python-dotenv
+
+3. Run the following code
+    
+    python your_script_name.py
